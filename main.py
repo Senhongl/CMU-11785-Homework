e059dcd92ad95db5a8851d8e68299dc8f56cc4f4
Ubuntu <ubuntu@ip-172-31-4-207.us-east-2.compute.internal>
2019-11-16 22:56:09 +0000

    add transform func

--- main.py
+++ main.py
@@ -17,3 +17,5 @@ train_loader = Data.DataLoader(train_data, **train_loader_args)
 dev_loader_args = dict(shuffle=False, batch_size = 256, pin_memory=True, collate_fn = collate_fn) 
 dev_loader = Data.DataLoader(dev_data, **dev_loader_args)
 
+letter_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',\
+             'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '-', "'", '.', '_', '+', ' ','<sos>','<eos>']
--- util.py
+++ util.py
@@ -1,2 +1,23 @@
 import numpy as np
 
+def transform_letter_to_index(transcript, letter_list):
+	'''
+	:param transcript : Transcripts are the text input
+	:param letter_list : Letter list defined above
+	:return letter_to_index_list : Returns a list for all the transcript sentence to index
+	'''
+	letter_to_index_list = []
+	for utterance in transcript:
+		letters = []
+		letters.append(letter_list.index('<sos>'))
+		for byte in utterance:
+			string = byte.decode('utf8')
+			
+			for character in string:
+				letters.append(letter_list.index(character))
+
+		letters.append(letter_list.index('<eos>'))
+		letter_to_index_list.append(letters)
+
+	return letter_to_index_list
+
